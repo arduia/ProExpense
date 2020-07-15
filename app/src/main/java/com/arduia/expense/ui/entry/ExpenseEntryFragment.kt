4d92c3400d87bede5f0f375a6ed56e0179051a86
arduia <agye.tech@gmail.com>
2020-07-15 13:14:29 +0630

    refactor: Entry

--- app/src/main/java/com/arduia/expense/ui/entry/ExpenseEntryFragment.kt
+++ app/src/main/java/com/arduia/expense/ui/entry/ExpenseEntryFragment.kt
@@ -19,11 +19,11 @@ import com.arduia.expense.databinding.FragExpenseEntryBinding
 import com.arduia.expense.ui.common.EventObserver
 import com.arduia.expense.ui.common.ExpenseCategoryProviderImpl
 import com.arduia.expense.ui.common.MarginItemDecoration
+import com.arduia.expense.ui.vto.ExpenseDetailsVto
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
-import java.lang.IllegalStateException
 
 class ExpenseEntryFragment : Fragment() {
 
@@ -65,6 +65,7 @@ class ExpenseEntryFragment : Fragment() {
         MainScope().launch(Dispatchers.Main) {
             val aniDuration = resources.getInteger(R.integer.entry_pop_up_duration)
             delay(aniDuration.toLong())
+
             afterAnimation()
         }
 
@@ -72,7 +73,8 @@ class ExpenseEntryFragment : Fragment() {
 
     private fun afterAnimation() {
 
-        categoryAdapter.submitList(categoryProvider.getCategoryList())
+        val categoryList = categoryProvider.getCategoryList()
+        categoryAdapter.submitList(categoryList)
 
         viewBinding.btnSave.isEnabled = true
 
@@ -116,19 +118,21 @@ class ExpenseEntryFragment : Fragment() {
         val sampleCategory = categoryProvider.getCategoryList().take(1)
         categoryAdapter.submitList(sampleCategory)
 
+
     }
 
     private fun setupViewModel() {
 
-        when {
-            args.expenseId > 0 -> {
-                // ID exist, Update Mode
-                viewModel.setUpdateMode()
-            }
+        val defaultExpenseID =  resources.getInteger(R.integer.default_expense_id)
 
-            args.expenseId <= 0 -> {
+        when (args.expenseId == defaultExpenseID) {
+            true -> {
                 // ID is default, Save Mode
                 viewModel.setSaveMode()
+            }
+            false -> {
+                // ID exist, Update Mode
+                viewModel.setUpdateMode()
             }
         }
 
@@ -141,14 +145,21 @@ class ExpenseEntryFragment : Fragment() {
             findNavController().popBackStack()
         })
 
+//        viewModel.isLoading.observe(viewLifecycleOwner, Observer {
+//             viewBinding.pbLoading.visibility = when(it){
+//                 true -> View.VISIBLE
+//                 false -> View.INVISIBLE
+//             }
+//        })
+
         viewModel.entryMode.observe(viewLifecycleOwner, EventObserver {
             when (it) {
                 ExpenseEntryMode.UPDATE -> {
-                    onUpdateMode()
+                    changeUpdateMode()
                 }
 
                 ExpenseEntryMode.INSERT -> {
-                    onInsertMode()
+                    changeSaveMode()
                 }
             }
         })
@@ -161,87 +172,63 @@ class ExpenseEntryFragment : Fragment() {
         })
 
         viewModel.selectCategory(categoryProvider.getCategoryByID(1))
-
     }
 
-
-    private fun onUpdateMode() {
-        viewBinding.tvEntryTitle.text = getString(R.string.label_update_data)
-        viewBinding.btnSave.text = getString(R.string.label_update)
-        viewBinding.btnSave.setOnClickListener {
-            updateData()
-        }
-        viewModel.observeExpenseData(args.expenseId)
+    private fun changeUpdateMode() = with(viewBinding) {
+        tvEntryTitle.text = getString(R.string.label_update_data)
+        btnSave.text = getString(R.string.label_update)
+        btnSave.setOnClickListener { updateData() }
+        viewModel.setExpenseData(args.expenseId)
     }
 
-    private fun onInsertMode() {
-        viewBinding.tvEntryTitle.text = getString(R.string.label_expense_entry)
-        viewBinding.btnSave.setOnClickListener {
-            saveData()
-        }
-        viewBinding.edtName.requestFocus()
+    private fun changeSaveMode() = with(viewBinding) {
+        tvEntryTitle.text = getString(R.string.label_expense_entry)
+        btnSave.text = getString(R.string.label_save)
+        btnSave.setOnClickListener { saveData() }
+        edtName.requestFocus()
     }
 
     private fun saveData() {
-        val name = viewBinding.edtName.text.toString()
-        val cost = viewBinding.edtAmount.text.toString()
-        val description = viewBinding.edtNote.text.toString()
-
+        val currentExpenseDetail = getExpenseDetail()
         //View Level Validation
-        if (cost.isEmpty()) {
+        if (currentExpenseDetail.amount.isEmpty()) {
             viewBinding.edtAmount.error = getString(R.string.label_cost_empty)
             return
         }
-
-        val category = categoryAdapter.selectedItem ?: return
-
-        viewModel.saveExpenseData(
-            name = name,
-            cost = cost.toLongOrNull() ?:
-            throw IllegalStateException("Entry cost is not a Decimal"),
-
-            description = description,
-            category = category.id
-        )
-
-        clearSpendSheet()
+        viewModel.saveExpenseData(currentExpenseDetail)
     }
 
     private fun updateData() {
+        val currentExpenseDetail = getExpenseDetail()
+        //View Level Validation
+        if (currentExpenseDetail.amount.isEmpty()) {
+            viewBinding.edtAmount.error = getString(R.string.label_cost_empty)
+            return
+        }
+        viewModel.updateExpenseData(currentExpenseDetail)
+    }
+
+    private fun getExpenseDetail(): ExpenseDetailsVto{
 
         val name = viewBinding.edtName.text.toString()
         val cost = viewBinding.edtAmount.text.toString()
         val description = viewBinding.edtNote.text.toString()
 
-        //View Level Validation
-        if (cost.isEmpty()) {
-            viewBinding.edtAmount.error = getString(R.string.label_cost_empty)
-            return
-        }
+        val category = categoryAdapter.selectedItem ?: throw Exception("Category Item is not selected!")
 
-        val category = categoryAdapter.selectedItem!!
-
-        viewModel.updateExpenseData(
-            id = args.expenseId,
-            name = name,
-            cost = cost.toLongOrNull()
-                ?: throw IllegalStateException("Entry cost is not a Decimal"),
-            description = description,
-            category = category.id
+        return ExpenseDetailsVto(
+            args.expenseId,
+            name,
+            "",
+            category.id,
+            cost,
+            "",
+            description
         )
-
-        clearSpendSheet()
-    }
-
-    private fun clearSpendSheet() {
-        viewBinding.edtName.setText("")
-        viewBinding.edtAmount.setText("")
-        viewBinding.edtNote.setText("")
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
-
         val inputMethodManager =
             (requireContext().getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager)
         inputMethodManager?.hideSoftInputFromWindow(viewBinding.edtName.windowToken, 0)
--- app/src/main/java/com/arduia/expense/ui/entry/ExpenseEntryViewModel.kt
+++ app/src/main/java/com/arduia/expense/ui/entry/ExpenseEntryViewModel.kt
@@ -7,6 +7,7 @@ import com.arduia.expense.data.local.ExpenseEnt
 import com.arduia.expense.di.ServiceLoader
 import com.arduia.expense.ui.common.*
 import com.arduia.expense.ui.mapping.ExpenseMapper
+import com.arduia.expense.ui.vto.ExpenseDetailsVto
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
@@ -31,6 +32,9 @@ class ExpenseEntryViewModel(private val app:Application) : AndroidViewModel(app)
     private val _selectedCategory = BaseLiveData<ExpenseCategory>()
     val selectedCategory get() = _selectedCategory.asLiveData()
 
+    private val _isLoading = BaseLiveData<Boolean>()
+    val isLoading get() = _isLoading
+
     private val serviceLoader by lazy {
         ServiceLoader.getInstance(app)
     }
@@ -51,46 +55,47 @@ class ExpenseEntryViewModel(private val app:Application) : AndroidViewModel(app)
         _entryMode post event(ExpenseEntryMode.INSERT)
     }
 
-    fun saveExpenseData(name: String,
-                        cost:Long,
-                        description: String,
-                        category: Int){
+    fun saveExpenseData(detail: ExpenseDetailsVto){
 
         viewModelScope.launch(Dispatchers.IO) {
-            val saveTransaction = ExpenseEnt(
-                name = name,
-                amount = cost,
-                note = description,
-                category = category,
+            _isLoading.postValue(true)
+            val saveExpenseEnt = ExpenseEnt(
+                name = detail.name,
+                amount = detail.amount.toLong(),
+                note = detail.note,
+                category = detail.category,
                 created_date = Date().time,
                 modified_date = Date().time
             )
-            accRepository.insertExpense(saveTransaction)
+            accRepository.insertExpense(saveExpenseEnt)
             _dataInserted post EventUnit
+            _isLoading.postValue(false)
         }
     }
 
-    fun updateExpenseData( id: Int,
-                      name: String,
-                      cost:Long,
-                      description: String,
-                      category: Int){
+    fun updateExpenseData(detail: ExpenseDetailsVto){
         viewModelScope.launch(Dispatchers.IO) {
-            val saveTransaction = ExpenseEnt(
-                expense_id = id,
-                name = name,
-                amount = cost,
-                note = description,
-                category = category,
+            _isLoading.postValue(true)
+            val updateExpenseEnt = ExpenseEnt(
+                expense_id = detail.id,
+                name = detail.name,
+                amount = detail.amount.toLong(),
+                note = detail.note,
+                category = detail.category,
                 created_date = expenseData.value?.date?: throw Exception("Created Date Should not be null"),
                 modified_date = Date().time
             )
-            accRepository.updateExpense(saveTransaction)
+            accRepository.updateExpense(updateExpenseEnt)
             _dataUpdated post EventUnit
+            _isLoading.postValue(false)
         }
     }
 
-    fun observeExpenseData(id: Int){
+    fun setExpenseData(id: Int){
+        observeExpenseData(id)
+    }
+
+    private fun observeExpenseData(id: Int){
 
         viewModelScope.launch(Dispatchers.IO){
             val repoData = accRepository.getExpense(id).first()
