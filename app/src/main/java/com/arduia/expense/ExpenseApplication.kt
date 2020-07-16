f7cbc93091dbdf6e520c8a61a290694844e2ec4b
Aung Ye Htet <agye.tech@gmail.com>
2020-07-17 00:52:03 +0630

    feat: Multi_Language Support

--- app/src/main/java/com/arduia/expense/ExpenseApplication.kt
+++ app/src/main/java/com/arduia/expense/ExpenseApplication.kt
@@ -1,7 +1,9 @@
 package com.arduia.expense
 
 import android.app.Application
+import android.content.Context
 import android.view.LayoutInflater
+import com.arduia.core.lang.updateResource
 import com.arduia.expense.data.local.AccountingDatabase
 import com.arduia.expense.data.local.AccountingDatabase_Impl
 import com.arduia.expense.databinding.FragExpenseEntryBinding
@@ -14,6 +16,11 @@ class ExpenseApplication: Application(){
     override fun onCreate() {
         super.onCreate()
         ExpenseCategoryProviderImpl(resources).init()
+    }
+
+    override fun attachBaseContext(base: Context?) {
+        val localedContext = base?.updateResource("my")
+        super.attachBaseContext(localedContext)
     }
 
 }
