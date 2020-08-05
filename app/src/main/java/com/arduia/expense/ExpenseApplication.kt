b77564c9c43f4ee3f6733e0211abcc4c41621eea
Aung Ye Htet <agye.tech@gmail.com>
2020-08-05 23:45:33 +0630

    refactor: ExpenseApplication

--- app/src/main/java/com/arduia/expense/ExpenseApplication.kt
+++ app/src/main/java/com/arduia/expense/ExpenseApplication.kt
@@ -2,49 +2,50 @@ package com.arduia.expense
 
 import android.app.Application
 import android.content.Context
-import android.content.res.Configuration
-import android.view.LayoutInflater
 import androidx.hilt.work.HiltWorkerFactory
 import com.arduia.core.lang.updateResource
-import com.arduia.expense.data.SettingsRepository
 import com.arduia.expense.data.SettingsRepositoryImpl
 import dagger.hilt.android.HiltAndroidApp
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import timber.log.Timber
-import java.util.*
 import javax.inject.Inject
 
 @HiltAndroidApp
-class ExpenseApplication: Application(), androidx.work.Configuration.Provider{
+class ExpenseApplication : Application(), androidx.work.Configuration.Provider {
 
     @Inject
     lateinit var workerFactory: HiltWorkerFactory
 
     override fun onCreate() {
         super.onCreate()
-        when(BuildConfig.DEBUG){
+        setupLogging()
+    }
+
+    private fun setupLogging() {
+        when (BuildConfig.DEBUG) {
             true -> Timber.plant(Timber.DebugTree())
-            false ->  Unit
+            false -> Unit
         }
     }
 
-    override fun getWorkManagerConfiguration(): androidx.work.Configuration  =
+    override fun getWorkManagerConfiguration(): androidx.work.Configuration =
         androidx.work.Configuration.Builder()
             .setWorkerFactory(workerFactory)
             .build()
 
     override fun attachBaseContext(base: Context?) {
-        runBlocking {
-            base?.let {
-                val settings = SettingsRepositoryImpl(base, GlobalScope)
-                val selectedLanguage = settings.getSelectedLanguage().first()
-                val localedContext = base.updateResource(selectedLanguage)
-                super.attachBaseContext(localedContext)
-            }
-        }
+        if (base == null) return
+        val updatedLocale = updateToLanguageContext(baseContext = base)
+        super.attachBaseContext(updatedLocale)
     }
+
+    private fun updateToLanguageContext(baseContext: Context): Context =
+        runBlocking {
+            val settings = SettingsRepositoryImpl(baseContext, GlobalScope)
+            val selectedLanguage = settings.getSelectedLanguage().first()
+            return@runBlocking baseContext.updateResource(selectedLanguage)
+        }
 
 }
